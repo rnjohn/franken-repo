6462eb294bcbd275568e0d34c42655251cea5c4a
rnjohn <jefopa99@gmail.com>
2024-03-17 13:48:56 -0500

    add links to async

--- node/async/async.js
+++ node/async/async.js
@@ -14,3 +14,7 @@ console.log('after mockRequest')
 // I had the doubt on why the keyword 'async' is async instead of 'sync' if it was making the code synchronous.
 // It isn't making the code synchronous. Only what is inside the async function has synchronous behavior but the
 // rest of the code is unnaffected.
+
+// Discussion
+// https://stackoverflow.com/questions/70048491/why-we-have-to-use-keyword-async-and-await-to-making-it-synchronus-in-node-when
+// https://stackoverflow.com/questions/72799361/are-async-await-async-or-synchronous
